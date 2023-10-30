0a61201c65398ea722c7e0ff3a389cd7233a3b16
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-10-30 19:15:42 -0400

    first commit

---
+++ src/main/java/com/jenkins/maven/assignment2/TeenaJenkinsMavenProjectApplication.java
@@ -0,0 +1,14 @@
+package com.jenkins.maven.assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenaJenkinsMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenaJenkinsMavenProjectApplication.class, args);
+		System.out.println("Welcome to COMP367");
+	}
+
+}
---
+++ src/test/java/com/jenkins/maven/assignment2/TeenaJenkinsMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.maven.assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenaJenkinsMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
